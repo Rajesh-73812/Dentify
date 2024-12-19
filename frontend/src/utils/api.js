f366c07c253766c7645898799e747b8fcfba1f0d
Rajesh-73812 <rajesh.swain@innoitlabs.com>
2024-12-19 11:55:57 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000',
-  baseURL: 'https://property-rental-backend-ten.vercel.app',
-  // baseURL: 'https://property-rental-backend-5.onrender.com', 
+  // baseURL: 'https://property-rental-backend-ten.vercel.app',
+  baseURL: 'https://property-rental-backend-5.onrender.com',
   withCredentials: true, 
 });
 
